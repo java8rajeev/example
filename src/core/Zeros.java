e38e0ecdc9581b42d5cec0458f4210c3843b92ed
Asus <Asus@DESKTOP-GDCC9UG>
2024-07-20 10:46:26 -0700

    Second Project

---
+++ src/core/Zeros.java
@@ -0,0 +1,14 @@
+package core;
+
+import java.util.Collection;
+
+
+public class Zeros  {
+	public static void main(String[] args) {
+		String str="00001111000";
+		System.out.println(str);
+		
+	}
+
+ 
+}
